c109358a89cfd28d812c1d526a5a053ea7f5de12
lucky <lucky@LAPTOP-3N4684IV>
2022-11-26 18:46:30 -0500

    made massive changes and started the neural network

--- retrieveData.py
+++ retrieveData.py
@@ -13,14 +13,16 @@ api = NewsApiClient(api_key='c8b7f53e7eca48adae6cdcc7e7361ea7')
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 headers = {'User-Agent':user_agent}
 # Making a call to the newsapi to ask for articles, in this case the query is 'politics'
-API_request = api.get_everything(q='politics',
+API_request = api.get_everything(q='election',
                                      # sources='bbc-news,the-verge', # You can specify sources
-                                     # domains='bbc.co.uk,techcrunch.com', # Or domains
-                                      from_param='2022-05-27', # The dates your pulling between
-                                      to='2022-06-27',
+                                      domains='foxnews.com, cnn.com, thefederalist.com, washingtonpost.com, huffpost.com, infowars.com, occupydemocrats.com, americanlibertyreportnews.com', # Or domains
+                                      from_param='2022-10-20', # The dates your pulling between
+                                      to='2022-10-27',
                                       language='en',
-                                      sort_by='relevancy',
-                                      page=2)
+                                      sort_by='relevancy'
+                                      #country='us'
+                                      #page=2
+                                      )
 
 # Making a list of the articles' urls to parse for their HTML
 numberOfArticles = len(API_request['articles']) 
@@ -28,6 +30,38 @@ articleCount = 0
 urlList = []
 for i in API_request['articles']:
     urlList.append(API_request['articles'][articleCount]['url'])
+    if(API_request['articles'][articleCount]['url'].__contains__('foxnews')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('thefederalist')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('infowars')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('americanlibertyreportnews')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('cnn')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('washingtonpost')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('huffpost')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleCount]['url'].__contains__('occupydemocrats')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
     articleCount = articleCount+1
 
 
@@ -43,7 +77,152 @@ for i in urlList:
         article_name = newspaper.Article(url=urlList[articleCount], language="en")
         article_name.download()
         article_name.parse()
-        f.write(article_name.text + "\n\nURL: " + urlList[articleCount] + "\n\nTITLE: " + article_name.title)
+        f.write(article_name.text)
+        
+    except:
+        print("Error opening link, probably dead")
+        continue
+    f.close()
+    articleCount = articleCount+1
+# new request
+
+API_request = api.get_everything(q='election',
+                                     # sources='bbc-news,the-verge', # You can specify sources
+                                      domains='foxnews.com, cnn.com, thefederalist.com, washingtonpost.com, huffpost.com, infowars.com, occupydemocrats.com, americanlibertyreportnews.com', # Or domains
+                                      from_param='2022-10-28', # The dates your pulling between
+                                      to='2022-11-04',
+                                      language='en',
+                                      sort_by='relevancy'
+                                      #country='us'
+                                      #page=2
+                                      )
+
+# Making a list of the articles' urls to parse for their HTML
+numberOfArticles = len(API_request['articles']) 
+articleOn = 0
+for i in API_request['articles']:
+    urlList.append(API_request['articles'][articleOn]['url'])
+    if(API_request['articles'][articleOn]['url'].__contains__('foxnews')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('thefederalist')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('infowars')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('americanlibertyreportnews')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('cnn')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('washingtonpost')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('huffpost')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('occupydemocrats')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    articleCount = articleCount+1
+    articleOn=articleOn+1
+
+articleCount = articleCount-numberOfArticles
+
+# For every url, do these things
+for i in urlList:
+    #req = Request(urlList[articleCount], None, headers)
+    # try to open the web page with an HTTP request
+    f = open("articles/article%d.txt" % articleCount, 'w', encoding="utf-8")
+    
+    try:
+        article_name = newspaper.Article(url=urlList[articleCount], language="en")
+        article_name.download()
+        article_name.parse()
+        f.write(article_name.text)
+        
+    except:
+        print("Error opening link, probably dead")
+        continue
+    f.close()
+    articleCount = articleCount+1
+
+# new request
+
+API_request = api.get_everything(q='election',
+                                     # sources='bbc-news,the-verge', # You can specify sources
+                                      domains='foxnews.com, cnn.com, thefederalist.com, washingtonpost.com, huffpost.com, infowars.com, occupydemocrats.com, americanlibertyreportnews.com', # Or domains
+                                      from_param='2022-11-05', # The dates your pulling between
+                                      to='2022-11-12',
+                                      language='en',
+                                      sort_by='relevancy'
+                                      #country='us'
+                                      #page=2
+                                      )
+
+# Making a list of the articles' urls to parse for their HTML
+numberOfArticles = len(API_request['articles']) 
+articleOn = 0
+for i in API_request['articles']:
+    urlList.append(API_request['articles'][articleOn]['url'])
+    if(API_request['articles'][articleOn]['url'].__contains__('foxnews')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('thefederalist')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('infowars')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('americanlibertyreportnews')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('cnn')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('washingtonpost')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('huffpost')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    if(API_request['articles'][articleOn]['url'].__contains__('occupydemocrats')):
+        g = open("articleBiases/article%dBias.txt" % articleCount, 'w', encoding="utf-8")
+        g.write("-1")
+        g.close()
+    articleCount = articleCount+1
+    articleOn=articleOn+1
+
+
+articleCount = articleCount-numberOfArticles
+# For every url, do these things
+for i in urlList:
+    #req = Request(urlList[articleCount], None, headers)
+    # try to open the web page with an HTTP request
+    f = open("articles/article%d.txt" % articleCount, 'w', encoding="utf-8")
+    
+    try:
+        article_name = newspaper.Article(url=urlList[articleCount], language="en")
+        article_name.download()
+        article_name.parse()
+        f.write(article_name.text)
         
     except:
         print("Error opening link, probably dead")
--- sanitizeData.py
+++ sanitizeData.py
@@ -1,12 +1,4 @@
-#import string
-#import nltk
-#import spacy
-#import sys
 import pandas as pd
-#import wordcloud
-#import lexical_diversity
-#import re
-#import collections
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
@@ -18,38 +10,63 @@ from wordcloud import WordCloud
 from lexical_diversity import lex_div as ld
 import os
 
-#fileCount = 0
-#directory = 'articles'
-#for fileName in os.listdir(directory):
-#    f = os.path.join(directory, fileName)
-#    if os.path.isfile(f):
-#        for 
-def countWordFrequency(pathInput):
+def countWordFrequency():
+    # An object used for taking the root of a word, i.e. 'programming', 'programmer', programs' are turned into 'program'
     ps = PorterStemmer()
+    # a list of characters that we want to seperate from text
     replacements = [",", ".", ":", "'", '"', "!", "?", "." ";", "@", "{", "}", "[", "]", "(", ")", "/", "#", "*", "_", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "%", "-", "£", "$", "&"]
-    for i in range(0,72):
+    
+    # 71 is abritrary, it's just the number of articles I was able to pull today
+    # In the future, it would be wise to just use the number of articles in the /articles directory as the number to iterate through
+    # We would then have to move the files out of the /articles and /cleanedArticles directories after processing
+    for i in range(0,300):
+        # A string to hold the contents of the article's contents when read
         text = ""
+        # Created a dictionary mapping words to the number of their occurances
         frequencies = {}
+        sortedFrequencies = {}
+
+        # Opening the article file, assigning text it's contents, and closing the file
         f = open("articles/article%d.txt" % i, 'r', encoding="utf-8")
         text = str(f.read()).lower()
         f.close()
+
+        # Going through each character we listed previously to create a space
         for r in range(0, len(replacements)):
+            # Creating a space here allows us to count it in the dictionary later
             text = text.replace(replacements[r], " " + replacements[r] + " ")
+        
+        # Splitting the string of text into an array of words
         text = text.split()
+
+        # I figure any word processed that's greater than 12 characters is a fluke
+        # I.e. a link, or a piece of HTML data that escaped the parsing of newspaper3k
         text = [word for word in text if len(word) < 12]
+
+        # For every word in the array...
         for word in text:
+            # take the 'root' of the word with our PorterStemmer object...
             word = ps.stem(word)
+
+            # if the word has been seen already, add one to the number of occurances...
             if word in frequencies:
                 frequencies[word] += 1
+            # if it's a new word, make a dictionary value to begin counting it
             else:
                 frequencies[word] = 1
+            
+        # Sorting the frequencies dictionary so its ranked in ascending order
         sortedD = sorted(frequencies, key=frequencies.get)
-        sortedFrequencies = {}
         for key in sortedD:
             sortedFrequencies[key] = frequencies[key]
+
+        # Opening the appropriately matching file in cleanedArticles directory
         f = open("cleanedArticles\Article%d.txt" % i, "w", encoding="utf-8")
+
+        # Writing the sortedFrequencies dictionary to the file, line by line
         f.write(str(sortedFrequencies).replace(', ',',\n '))
         f.close()
+    # Return statement useful for debugging
     return sortedFrequencies
 
-print(countWordFrequency('cleanedArticles/article0.txt'))
+countWordFrequency()
